2f97eef1c814efede1033f4ce31bbb24906c6318
Vibol T <vibolteav@gmail.com>
2021-07-07 00:19:22 +0700

    downgrade ruby

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 set :deploy_to, "/var/www/#{fetch(:application)}"
 set :pty, false
 
-set :rvm_ruby_version, '2.7.2@lays-s2'
+set :rvm_ruby_version, '2.5.0@lays-s2'
 set :rvm_roles, [:app, :web]
 set :passenger_restart_with_touch, true
 
