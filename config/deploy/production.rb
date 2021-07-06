df038d0390dc44f88846e4abdf8c05dd91fd0d41
Vibol T <vibolteav@gmail.com>
2021-07-07 01:22:44 +0700

    updated production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,5 +3,6 @@ set :stage, :production
 set :branch, 'main'
 set :repo_url, "git@github.com:tvcam/#{fetch(:application)}.git"
 set :linked_files, fetch(:linked_files, []).push('db/production.sqlite3')
+set :linked_files, fetch(:linked_files, []).push('config/credentials/production.key')
 
 server '52.53.239.219', user: 'deployer', roles: %w{app web db}
