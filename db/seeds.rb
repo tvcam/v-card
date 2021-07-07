07f446fd44f9960f5ca1dfff235091c823d9ec51
Vibol T <vibolteav@gmail.com>
2021-07-07 09:11:47 +0700

    updated UI + i18n

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: 'vibolteav.ds@gmail.com', password: '8Fu^ny2p3er', password_confirmation: '8Fu^ny2p3er') if Rails.env.development?
